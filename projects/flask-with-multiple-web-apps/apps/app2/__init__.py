2b35d7992374066a495eb9af1d1c80047375c335
paramraghavan <paramraghavan@gmail.com>
2025-04-06 22:50:30 -0400

    Revert "flask app with mutiple web apps support, deployed"
    This reverts commit ae67755195453f69021e75febc68189c2c2b3b28.

--- projects/flask-with-multiple-web-apps/apps/app2/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Create the blueprint
-app2 = Blueprint('app2', __name__, template_folder='templates', url_prefix='/app2')
-
-# Define a function to initialize routes
-def init_app():
-    from apps.app2 import routes
-    return app2
--- projects/flask-with-multiple-web-apps/config.py
+++
@@ -1,6 +0,0 @@
-# First, let's create the config.py file
-# config.py
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    DEBUG = True
-    # Add other configuration settings as needed
